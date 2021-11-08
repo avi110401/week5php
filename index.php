59d3cba49d8ef3baf2b6f6a251f9dd5be8237751
Avirup Biswas <63997292+avi110401@users.noreply.github.com>
2021-11-08 17:42:54 +0530

    uploaded files

---
+++ index.php
@@ -0,0 +1,18 @@
+<title>Avirup Biswas PHP</title>
+<h1>Avirup Biswas PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Avirup Biswas\" is ".hash('sha256', 'Avirup Biswas');
+?></p>
+<pre>ASCII ART:
+	       ***
+	      *****
+	     **	  **
+	    **     **
+	   ***********
+	  *************
+	 **           **
+	**             **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
